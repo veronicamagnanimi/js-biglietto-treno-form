35c3bd19c8993261c0eb38e705a0b960ecc4398e
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-09 15:14:27 +0100

    feat: complete milestone 2

--- js/script.js
+++ js/script.js
@@ -9,6 +9,9 @@ const userAge = document.getElementById("age");
 const usernameElem = document.getElementById("name-user");
 const priceElem = document.getElementById("final-price");
 const cardElem = document.querySelector(".card");
+const offerElem = document.getElementById("offer");
+const carriageElem = document.getElementById("carriage");
+const codeElem = document.getElementById("code");
 
 
 //funzione callback
@@ -22,30 +25,57 @@ formElem.addEventListener("submit", function(event) {
   const age = parseInt(userAge.value);
 
 
-  console.log(totalPrice(age, km));
+//   console.log(totalPrice(age, km));
 
   cardElem.classList.remove("d-none");
+  formElem.reset();
 
+  //innerHTML
   usernameElem.innerHTML = username;
-  priceElem.innerHTML = "€" + totalPrice(age, km);
+  priceElem.innerHTML = "€ " + totalPrice(age, km).toFixed(2);
+  offerElem.innerHTML = typeOffer(age);
+  carriageElem.innerHTML = randomNum(1, 5);
+  codeElem.innerHTML = randomNum(1000, 2000);
   });
 
 
-//   calcolo prezzo biglietto
 
+
+
+////////FUNZIONI///////////
+
+//funzione prezzo * età * km
     const totalPrice = (eta, kilometri) => {
     let sconto = 0;
     let prezzo = 0;
     let priceKm = 0.21;
    
-
     if(eta < 18) {
         sconto = 20;
     } else if (eta >= 65) {
         sconto = 40;
     }
     return prezzo = priceKm * kilometri * (100 - sconto) / 100;
+    }
     
+    
+
+
+//funzione numero random
+  function randomNum(min, max) {
+    return(Math.floor(Math.random() * (max-min) + min));
+  }
+
+
+//funzione tipo di offerta
+function typeOffer (age) {
+    let offerType = "Biglietto Standard";
+    if (age < 18) {
+        offerType = "Sconto Under18";
+    } else if (age > 65) {
+        offerType = "Sconto Over65";
+    }
+    return offerType;
 }
 
 
